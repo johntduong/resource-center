cba98efe255d35402e7a8bbce7c569a0ebff9dda
johntduong <johntduong@yahoo.com>
2017-09-28 23:11:43 -0700

    added drawer for menu component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import AppBar from 'material-ui/AppBar';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+import AppBar from 'material-ui/AppBar'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {open: false};
+  }
+
   render() {
     return (
       <div className="App">
         <AppBar
           title="Title"
           iconClassNameRight="muidocs-icon-navigation-expand-more"
+          onLeftIconButtonTouchTap={() => this.setState({open: !this.state.open})}
         />
+        <Drawer open={this.state.open}>
+          <MenuItem>Menu Item</MenuItem>
+          <MenuItem>Menu Item 2</MenuItem>
+        </Drawer>
       </div>
     );
   }
